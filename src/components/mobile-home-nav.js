47a2fed2710aa5131b85d9d0225818e92e277c9a
jjward25 <45015765+jjward25@users.noreply.github.com>
2021-08-24 19:04:28 -0600

    c

--- src/components/mobile-home-nav.js
+++ src/components/mobile-home-nav.js
@@ -7,12 +7,10 @@ function MobileHomeNav() {
     return (
     <div className='home-nav'>
         <Link to='/portfolio' className='nav-link'><div className='nav-item'>
-            <div className='gear-img'><img src={gear} alt='gear' id='gear-pic'/></div>
             <div className='nav-text'>{'Testimonials'}</div>
         </div></Link>
 
         <Link to='/about' className='nav-link'><div className='nav-item'>
-            <div className='gear-img'><img src={gear} alt='gear' id='gear-pic'/></div>
             <div className='nav-text'>About Joe</div>
         </div></Link>
     </div>
