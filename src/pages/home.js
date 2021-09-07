fe643201149d0f2d99662c414d3c2ac4a5d4b9a4
jjward25 <45015765+jjward25@users.noreply.github.com>
2021-09-07 17:23:37 -0600

    blue

--- src/pages/home.js
+++ src/pages/home.js
@@ -15,7 +15,8 @@ import pmp_logo from '../assets/work-pmp.png';
 import schwab from '../assets/work-schwab.png';
 import tda from '../assets/work-tda.png';
 import procurementDash from '../assets/portfolio-tableau-procurement.png';
-import adeptWireframe from '../assets/portfolio-figma-adept.png';
+import shopifyDash from '../assets/portfolio-tableau-shopify.PNG';
+import pythonAPI from '../assets/portfolio-python-api.PNG';
 import adeptProcessMap from '../assets/portfolio-figma-process-map.PNG';
 import whitepaper from '../assets/portfolio-whitepaper.PNG';
 import webscraper from '../assets/portfolio-webscraper.PNG';
@@ -59,52 +60,22 @@ return (
                     <p className='home-text-block'>These are some of the things I can help with as a consultant and experienced project manager.  Working as a team, we will identify, prioritize, and implement creative and custom solutions to your team's biggest problems. </p>
                 </div>
             </OnAtLeastTablet>
-
-            <OnMobile><div className='job-buttons'> 
-                    <div className='icon-header'>Past Employment</div>
-                    <div className='work-icon-wrap'>    
-                        <img src={schwab} alt='schwab' id='work-icon' title='Charles Schwab'/>
-                        <img src={tda} alt='tda' id='work-icon' title='TD Ameritrade'/>
-                        <img src={pmp_logo} alt='pmp' id='work-icon' title='Practice Makes Perfect'/>
-                    </div>
-            </div></OnMobile>
         
-            <div className= 'skill-grid'>
-                <OnAtLeastTablet>
-                    <div className='all-logos-wrap'>    
-                        <div className='work-icon-wrap'>
-                            <div className='icon-header'>Past<br></br>Employment</div>
-                            <img src={schwab} alt='schwab' id='work-icon' title='Charles Schwab'/>
-                            <img src={tda} alt='tda' id='work-icon' title='TD Ameritrade'/>
-                            <img src={pmp_logo} alt='pmp' id='work-icon' title='Practice Makes Perfect'/>
-                        </div>
-                        <div className='skill-icon-wrap'>
-                            <div className='icon-header'>{'Skills & Certs'}</div>
-                            <img src={tableau} alt='tableau' id='services-skill-button' title='Tableau'/>
-                            <img src={python} alt='python' id='services-skill-button' title='Python'/>
-                            <img src={sql} alt='sql' id='services-skill-button' title='SQL'/>
-                            <img src={sixsig} alt='six-sigma' id='services-skill-button' title='Lean/Six-Sigma'/>
-                            <img src={pmp_pmi} alt='pmp' id='services-skill-button' title='PMP Project Managemnt Professional'/>
-                        </div>
-                    </div>
-                </OnAtLeastTablet>
+            <div className= 'skill-grid'> 
 
                <div className='skill-main'>
-
                     <div className='home-text-wrap'>
-                        <div className='skill-header'>Sample Technical Projects</div>
-                        <li className='skill-list'>Creating an update-able reporting environment using available data and free tools.</li>
-                        <li className='skill-list'>Pulling data via python webscrapers or public APIs; combining with industry research for ad hoc analyses.</li>
+                        <div className='skill-header'>Sample Projects: Everything Ops</div>
+                        <li className='skill-list'>Creating a stable reporting environment using available data and free tools. </li>
+                        <li className='skill-list'>Leveling-up standard reporting from Shopify and other platforms using advanced Tableau visuals.</li>
+                        <li className='skill-list'>Pulling external data via python webscrapers or public APIs; combining with industry research for ad hoc analyses.</li>
                         <li className='skill-list'>One-time internal deep-dives; measuring effiency, process times, wait times, whatever's available to dig for opportunities.</li>
+                        <li className='skill-list'>Sitting down with team members, mapping out daily processes and finding and implementing efficiency-focused solutions (task automation or re-design) in a transparent and inclusive way.</li>
+                        <li className='skill-list'>Conducted sensitive interviews to determine if merging two teams was workable and in everyone's best interest.</li>
+                        <li className='skill-list'>{'Strategic Reviews: Individual and team interviews > process mapping > rough solutions design > research industry best practices > review and final design > implementation.'}</li>                        
+
                     </div>
-                    <OnMobile><div className='skill-logos'>
-                        <div className='skill-header'>{'Skills & Certs'}</div>
-                        <div className='skill-icon-wrap'>
-                            <img src={tableau} alt='tableau' id='services-skill-button' title='Tableau'/>
-                            <img src={python} alt='python' id='services-skill-button' title='Python'/>
-                            <img src={sql} alt='sql' id='services-skill-button' title='SQL'/>
-                        </div>
-                    </div></OnMobile>
+    
                     <div className='carousel-wrap'>
                         {/* eslint-disable-next-line*/} 
                         <Carousel className='carousel-inner-wrap' showArrows={true} showThumbs={false} showArrows={true} infiniteLoop={true} centerMode={.33} swipeable={true}>
@@ -114,9 +85,24 @@ return (
                                 <div className='legend'>Analytics Dashboard</div>
                             </div></a>
                             {/* eslint-disable-next-line*/} 
-                            <a href='https://i.imgur.com/UB8AKQB.png' target='_blank'><div className='carousel-item'>
-                                <img src={adeptWireframe} alt='adept' id='portfolio-img'/>
-                                <div className='legend'>Website Wireframe</div>
+                            <a href='https://i.imgur.com/DGgdJD4.png' target='_blank'><div className='carousel-item'>
+                                <img src={adeptProcessMap} alt='process map' id='portfolio-img'/>
+                                <div className='legend'>{'Process Mapping & Design'}</div>
+                            </div></a>
+                            {/* eslint-disable-next-line*/} 
+                            <a href='https://github.com/jjward25/python.crypto-data' target='_blank'><div className='carousel-item'>
+                                <img src={pythonAPI} alt='crypto-api-python' id='portfolio-img'/>
+                                <div className='legend'>Crypto Market Data API</div>
+                            </div></a>
+                            {/* eslint-disable-next-line*/} 
+                            <a href='https://imgur.com/a/uMF7jUq' target='_blank'><div className='carousel-item'>
+                                <img src={shopifyDash} alt='tableau-shopify' id='portfolio-img'/>
+                                <div className='legend'>Advanced Shopify Reporting</div>
+                            </div></a>
+                            {/* eslint-disable-next-line*/} 
+                            <a href='https://medium.com/cultural-commentary/strategic-resiliency-in-large-corporations-6003c35f98f6' target='_blank'><div className='carousel-item'>
+                                <img src={whitepaper} alt='adept' id='portfolio-img'/>
+                                <div className='legend'>Business Strategy</div>
                             </div></a>
                             {/* eslint-disable-next-line*/} 
                             <a href='https://github.com/jjward25/python-ca_scrapers' target='_blank'><div className='carousel-item'>
@@ -125,37 +111,22 @@ return (
                             </div></a>
                         </Carousel>
                     </div>
-
-                    <div className='home-text-wrap'>
-                        <div className='skill-header'>Sample Consulting Projects</div>
-                        <li className='skill-list'>Sitting down with team members, mapping out daily processes and finding and implementing efficiency-focused solutions (task automation or re-design) in a transparent and inclusive way.</li>
-                        <li className='skill-list'>Conducted sensitive interviews to determine if merging two teams was workable and in everyone's best interest.</li>
-                        <li className='skill-list'>Building out new procurement channels for ~$17m worth of annual spend in a cross-functional project affecting company-wide policy at TD Ameritrade.</li>
-                        <li className='skill-list'>{'Strategic Reviews: Individual and team interviews > process mapping > rough solutions design > research industry best practices > review and final design > implementation.'}</li>                        
-                    </div>
-                    <OnMobile><div className='skill-logos'>
-                        <div className='skill-header'>{'Skills & Certs'}</div>
-                        <div className='skill-icon-wrap'>
-                            <img src={sixsig} alt='six-sigma' id='services-skill-button' title='Lean/Six-Sigma'/>
-                            <img src={pmp_pmi} alt='pmp' id='services-skill-button' title='PMP Project Managemnt Professional'/>
-                        </div>
-                     </div></OnMobile>
-                    <div className='carousel-wrap'>
-                        {/* eslint-disable-next-line*/} 
-                        <Carousel  className='carousel-inner-wrap' showArrows={true} showThumbs={false} showArrows={true} infiniteLoop={true} centerMode={.33} swipeable={true}>
-                            {/* eslint-disable-next-line*/} 
-                            <a href='https://medium.com/cultural-commentary/strategic-resiliency-in-large-corporations-6003c35f98f6' target='_blank'><div className='carousel-item'>
-                                <img src={whitepaper} alt='adept' id='portfolio-img'/>
-                                <div className='legend'>Business Strategy</div>
-                            </div></a>
-                            {/* eslint-disable-next-line*/} 
-                            <a href='https://i.imgur.com/DGgdJD4.png' target='_blank'><div className='carousel-item'>
-                                <img src={adeptProcessMap} alt='process map' id='portfolio-img'/>
-                                <div className='legend'>{'Process Mapping & Design'}</div>
-                            </div></a>
-                        </Carousel>
-                    </div>
                 </div>
+            </div>
+            <div className='icon-header'>Past Employment</div>
+            <div className='work-icon-wrap'>    
+                <img src={schwab} alt='schwab' id='work-icon' title='Charles Schwab'/>
+                <img src={tda} alt='tda' id='work-icon' title='TD Ameritrade'/>
+                <img src={pmp_logo} alt='pmp' id='work-icon' title='Practice Makes Perfect'/>
+            </div>
+
+            <div className='icon-header'>{'Skills & Certs'}</div>
+            <div className='skill-icon-wrap'>
+                <img src={tableau} alt='tableau' id='services-skill-button' title='Tableau'/>
+                <img src={python} alt='python' id='services-skill-button' title='Python'/>
+                <img src={sql} alt='sql' id='services-skill-button' title='SQL'/>
+                <img src={sixsig} alt='six-sigma' id='services-skill-button' title='Lean/Six-Sigma'/>
+                <img src={pmp_pmi} alt='pmp' id='services-skill-button' title='PMP Project Managemnt Professional'/>
             </div>
         </div>
     </div>
