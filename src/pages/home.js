441c4fafab0070946a1e3f34d63557c7980c4c3f
jjward25 <45015765+jjward25@users.noreply.github.com>
2021-09-18 13:06:37 -0600

    g

--- src/pages/home.js
+++ src/pages/home.js
@@ -56,8 +56,8 @@ return (
                 </OnAtLeastTablet>
                 <OnAtLeastTablet className='home-text-wrapper'> 
                     <div className='home-text'>
-                        <p className='home-text-block'>Do you need help maximizing your data? Maybe you're looking for task automation, process reviews, or a one-off strategic analysis? </p>
-                        <p className='home-text-block'>These are some of the things I can help with as a consultant and experienced project manager.  Working as a team, we will identify, prioritize, and implement creative and custom solutions to your team's biggest problems. </p>
+                        <p className='home-text-block'>Do you need help maximizing your data? Maybe you're looking for task automation, process reviews, or a one-off strategic analysis? These are some of the things I can help with as a consultant and experienced project manager.</p>
+                        <p className='home-text-block'>Working as a team, we will identify, prioritize, and implement creative and custom solutions to your team's biggest problems. </p>
                     </div>
                 </OnAtLeastTablet>
                 <div className= 'skill-grid'> 
