f00c84ec96c7baee33dd1c992b31d7d52fe341fe
jjward25 <45015765+jjward25@users.noreply.github.com>
2021-10-15 17:58:15 -0600

    testimonialAustin

--- src/pages/testimonials.js
+++ src/pages/testimonials.js
@@ -1,11 +1,12 @@
 import React from 'react'; 
-import './main.css';
+import './testimonial.css';
 import Sidebar from '../components/sidebar';
 import { layoutGenerator } from 'react-break';
 import MobileMenu from '../components/mobile-menu';
 import bulb from '../assets/img-bulb.png';
 import tree from '../assets/img-tree.png';
 import biz from '../assets/img-biz.png';
+import perkLogo from '../assets/test-logo-perk.png'
 
 const layout = layoutGenerator({
     mobile: 0,
@@ -19,7 +20,7 @@ const layout = layoutGenerator({
   
   function Testimonials() {
   return (
-    <div className='testimonials-wrap'>
+    <div className='testimonials-page-wrap'>
 
         <OnMobile><MobileMenu/></OnMobile>
 
@@ -30,18 +31,31 @@ const layout = layoutGenerator({
         <OnAtLeastTablet><div></div></OnAtLeastTablet>
     
         <div className='testimonials-content'>
-          <OnAtLeastTablet><div className='border-grid'>
-              <div className='border-box-home'></div>
-              <div className='border-images-home'>
-                  <img src={bulb} alt='bulb' id='border-pic-home'/>
-                  <img src={tree} alt='tree' id='border-pic-home'/>
-                  <img src={biz} alt='biz' id='border-pic-home'/>
-              </div>
-          </div></OnAtLeastTablet>
-          <div className='testimonial-body'></div>
-              <div className='testimonial-title'>Testimonals Coming Soon :)</div>
-          </div>
+            
+            <OnAtLeastTablet><div className='border-grid'>
+                <div className='border-box-home'></div>
+                <div className='border-images-home'>
+                    <img src={bulb} alt='bulb' id='border-pic-home'/>
+                    <img src={tree} alt='tree' id='border-pic-home'/>
+                    <img src={biz} alt='biz' id='border-pic-home'/>
+                </div>
+            </div></OnAtLeastTablet>
 
+            <div className='testimonial-wrap'>
+            
+                <div className='testimonial'>
+                    <img src={perkLogo} alt='The Perk' id='testimonial-logo'/>
+                    <div className='testimonial-client'>The Perk</div>
+                    <div className='testimonial-description'>Built a Custom Dashboard Using Free Tools and Statistical Methods to Analyze Shopify Store Data</div>
+                    <div className='testimonial-text'>Joe worked his magic to format my Shopify data in a way that was super easy for me to understand. I immediately began to see trends correlated specifically to my marketing efforts throughout the last year. Joe's easy to understand analytics gave me motivation to continue investing time into marketing efforts to grow my business.</div>
+
+                    <div className='testimonial-example'>
+                        <img src='https://i.imgur.com/axXKT0E.png' alt='Perk Dashboard' id='testimonial-img'/>
+                    </div>
+                </div>
+
+            </div>
+        </div>
     </div>
   )};
 
