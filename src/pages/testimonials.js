419df5adee5f61a823609b4d6d3d7b090095afed
jjward25 <45015765+jjward25@users.noreply.github.com>
2021-10-15 19:04:43 -0600

    styleTouches

--- src/pages/testimonials.js
+++ src/pages/testimonials.js
@@ -47,7 +47,7 @@ const layout = layoutGenerator({
                     <img src={perkLogo} alt='The Perk' id='testimonial-logo'/>
                     <div className='testimonial-client'>The Perk</div>
                     <div className='testimonial-description'>Built a Custom Dashboard Using Free Tools and Statistical Methods to Analyze Shopify Store Data</div>
-                    <div className='testimonial-text'>Joe worked his magic to format my Shopify data in a way that was super easy for me to understand. I immediately began to see trends correlated specifically to my marketing efforts throughout the last year. Joe's easy to understand analytics gave me motivation to continue investing time into marketing efforts to grow my business. <br/><br/>- <em>Austin Gray, Owner</em></div>
+                    <div className='testimonial-text'>"Joe worked his magic to format my Shopify data in a way that was super easy for me to understand. I immediately began to see trends correlated specifically to my marketing efforts throughout the last year. Joe's easy to understand analytics gave me motivation to continue investing time into marketing efforts to grow my business." <br/><br/>- <em>Austin Gray, Owner</em></div>
 
                     <a href='https://imgur.com/a/uMF7jUq' className='testimonial-example' target='_blank' rel='noreferrer'>
                         <img src='https://i.imgur.com/axXKT0E.png' alt='Perk Dashboard' id='testimonial-img'/>
